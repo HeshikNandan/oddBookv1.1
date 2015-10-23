28aa82c4d714bea0731c97b85aba9f7be0889aab
Zavrick <soh.zi.haur@gmail.com>
2015-10-23 15:01:43 +0800

    Test Google Chart

--- client/oddBook.v1.1.js
+++ client/oddBook.v1.1.js
@@ -5,52 +5,53 @@ Activity = new Mongo.Collection("activity");
 Comments = new Mongo.Collection("comments");
 Chat = new Mongo.Collection("chat");
 
-if (Meteor.isClient) {
+Meteor.startup(function(){
+});
 
-  // Template.showUser.helpers({
-  //     user: {
-  //       name:
-  //     }
+
+// Template.showUser.helpers({
+//     user: {
+//       name:
+//     }
+//
+// });
+
+//Set value to the select option
+Template.createUser.helpers({
+  user: function(){
+    return User.find();
+  }
+});
+Template.body.events({
+    "click .submit-user": function (event) {
+      // Prevent default browser form submit
+      console.log("isExecuted");
+      event.preventDefault();
+      // Create all values
+      var email = $("#email").val();
+      var occupation = $("#occupation").val();
+      var birthday = $("#birthday").val();
+      var name = $("#name").val();
+      var location = $("#location").val();
+      var relationshipstatus = $("#relationshipstatus").val();
+      var status = $("#status").val();
+      var hobbies = $("#hobbies").val();
+      var boss = $("#bossid option:selected").attr("value");
+      // Insert a task into the collection
+      User.insert({
+        email: email,
+        occupation: occupation,
+        birthday: birthday,
+        name: name,
+        location: location,
+        relationshipstatus: relationshipstatus,
+        status: status,
+        hobbies: hobbies,
+        boss: boss,
+        createdAt: new Date() // current time
+      });
   //
-  // });
-
-  //Set value to the select option
-  Template.createUser.helpers({
-    user: function(){
-      return User.find();
+      // Clear form
+      //event.target.text.value = "";
     }
   });
-  Template.body.events({
-      "click .submit-user": function (event) {
-        // Prevent default browser form submit
-        console.log("isExecuted");
-        event.preventDefault();
-        // Create all values
-        var email = $("#email").val();
-        var occupation = $("#occupation").val();
-        var birthday = $("#birthday").val();
-        var name = $("#name").val();
-        var location = $("#location").val();
-        var relationshipstatus = $("#relationshipstatus").val();
-        var status = $("#status").val();
-        var hobbies = $("#hobbies").val();
-        var boss = $("#bossid option:selected").attr("value");
-        // Insert a task into the collection
-        User.insert({
-          email: email,
-          occupation: occupation,
-          birthday: birthday,
-          name: name,
-          location: location,
-          relationshipstatus: relationshipstatus,
-          status: status,
-          hobbies: hobbies,
-          boss: boss,
-          createdAt: new Date() // current time
-        });
-    //
-        // Clear form
-        //event.target.text.value = "";
-      }
-    });
-}
--- client/org_chart.js
+++ client/org_chart.js
@@ -1,17 +1,17 @@
-Template.orgChart.onCreated( function() {
+Template.orgChart.onRendered( function() {
   // chart = {
   //   target: 'chart',
-  //   type: 'BarChart',
+  //   type: 'orgchart',
   //   columns: [
-  //     ['string', 'Topping'],
-  //     ['number', 'Slices']
+  //     ['string', 'Name'],
+  //     ['string', 'Boss']
   //   ],
   //   rows: [
-  //     ['Mushrooms', 3],
-  //     ['Onions', 1],
-  //     ['Olives', 1],
-  //     ['Zucchini', 1],
-  //     ['Pepperoni', 2]
+  //     ['Vegetables', ''],
+  //     ['Lettuce', 'Vegetables'],
+  //     ['Fruits', ''],
+  //     ['Orange', 'Fruits'],
+  //     ['Banana', 'Fruits']
   //   ],
   //   options: {
   //     'title':'How Much Pizza I Ate Last Night',
@@ -21,4 +21,28 @@ Template.orgChart.onCreated( function() {
   // };
   //
   // drawChart(chart);
+  $.getScript('https://www.google.com/jsapi', function(){
+    console.log('loaded');
+    google.load('visualization', '1.1', {'packages':['orgchart'], callback: drawChart});
+
+    function drawChart() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Name');
+      data.addColumn('string', 'Manager');
+      data.addColumn('string', 'ToolTip');
+
+      data.addRows([
+        [{v:'Mike', f:'Mike<div style="color:red; font-style:italic">President</div>'}, '', 'The President'],
+        [{v:'Jim', f:'Jim<div style="color:red; font-style:italic">Vice President</div>'}, 'Mike', 'VP'],
+        ['Alice', 'Mike', ''],
+        ['Bob', 'Jim', 'Bob Sponge'],
+        ['Carol', 'Bob', '']
+      ]);
+      console.log(data);
+      var chart = new google.visualization.OrgChart(document.getElementById('chart'));
+      chart.draw(data, {allowHtml:true});
+    }
+  });
+
+
 });
